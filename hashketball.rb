5ee685920fa7e9bcb4b7f966a7b1d89f0e8db7a3
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-15 21:34:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,10 +204,7 @@ max_size = 0
 counter = 0
 data.each do |home_away, stats|
   stats[:players].each do |stats2|
-     while counter < data.length do
-
-       counter += 1
-     end
+    
   end
 end
 
