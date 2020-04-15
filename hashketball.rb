b040111bffa667a6d2890629207b0d6f80836225
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-15 18:55:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,8 +133,8 @@ def num_points_scored(player_name)
 
   data.each do |home_away, stats|
     stats.each do |team_color_player, stats_tcp|
-      stats_tcp.each do ||
-        
+      team_color_player.each do ||
+
       end
     end
   end
