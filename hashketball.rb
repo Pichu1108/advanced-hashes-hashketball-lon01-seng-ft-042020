6a803ade18fef512534ce5c653d10ef3edce7c90
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-15 19:23:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,12 +129,11 @@ end
 # Write code here
 def num_points_scored(player_name)
   data = game_hash
-  new_hash = {}
-  value = {}
+  new_hash =
   data.each do |home_away, stats|
     stats.each do |team_color_player, stats_tcp|
       if data[home_away][team_color_player] == :players
-        data[home_away][team_color_player][:players][:player_name]
+        new_hash.push(data[home_away][team_color_player][:players][:player_name])
       end
         binding.pry
     end
